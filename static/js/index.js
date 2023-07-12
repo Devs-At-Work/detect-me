2e15094a591995a6f5d90fb2379524ab89d90ef9
Ashutosh Kapoor <500097111@stu.upes.ac.in>
2023-07-12 18:46:54 +0530

    revamped: header, services, about

--- static/js/index.js
+++ static/js/index.js
@@ -4,3 +4,17 @@ const navbarLinks = document.getElementsByClassName('navbar-link')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    var navbarLink = document.querySelector('.navbar-link');
+  
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+      navbarLink.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+      navbarLink.classList.remove('scrolled');
+    }
+  });
